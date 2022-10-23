4c04ca4caceed2139a5c98d1cfe3c689ac5d8f5a
Jinlei Li <jinleili0@outlook.com>
2022-10-23 16:13:42 +0800

    CameraX preview

--- Android/app/src/main/java/name/jinleili/gpuimage4/MainActivity.kt
+++ Android/app/src/main/java/name/jinleili/gpuimage4/MainActivity.kt
@@ -1,9 +1,18 @@
 package name.jinleili.gpuimage4
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,21 +25,65 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import name.jinleili.gpuimage4.ui.theme.GPUImage4Theme
 
 class MainActivity : ComponentActivity() {
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+    private var cameraX: CameraX = CameraX(this, this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GPUImage4Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SurfaceCard()
+                    if (shouldShowCamera.value) {
+                        AndroidView(
+                            modifier = Modifier.fillMaxSize(),
+                            factory = { cameraX.startCameraPreviewView() }
+                        )
+                    } else {
+                        SurfaceCard()
+                    }
                 }
             }
+        }
+
+        requestCameraPermission()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        shouldShowCamera.value = isGranted
+        if (isGranted) {
+            Log.i("wgpu", "Permission granted")
+        } else {
+            Log.i("wgpu", "Permission denied")
+        }
+    }
+
+    // https://www.kiloloco.com/articles/015-camera-jetpack-compose/
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                shouldShowCamera.value = true
+                Log.i("wgpu", "Permission previously granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> Log.i("wgpu", "Show camera permissions dialog")
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
     }
 }
@@ -63,6 +116,7 @@ fun SurfaceCard() {
                 .height(screenWidth),
         )
     }
+
 }
 
 @Preview(showBackground = true)
